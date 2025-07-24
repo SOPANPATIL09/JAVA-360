6f5264bbae32f9e5ca1c512e90c1705b317baafa
sopan pramod patil <143089038+SOPANPATIL09@users.noreply.github.com>
2025-07-24 23:10:23 +0530

    Add files via upload
    unary operation (post-fix and pre-fix )

---
+++ Program3.java
@@ -0,0 +1,46 @@
+//operator 
+//unary operator required single operands
+//prefix
+//pre-increment ++a
+
+public class Program3{
+public static void main(String args[]){
+
+int a=10;
+System.out.println(a);
+++a;
+System.out.println(a);
+
+  
+//pre- decrement 
+
+int b=10;
+System.out.println(b);
+--b;
+System.out.println(b);
+
+
+
+//post-fix 
+System.out.println("postfix operation");
+
+//post- increment operator
+int d=10;
+System.out.println(d);
+
+d++;
+System.out.println(d);
+
+//post- decrement operator 
+  int e=10;
+  System.out.println(e);
+  e--;
+  System.out.println(e);
+   
+
+
+
+
+
+
+}} 
