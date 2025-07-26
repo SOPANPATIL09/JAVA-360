765cf8ac0e50109e182a9ecb527333f49e0a8305
sopan pramod patil <143089038+SOPANPATIL09@users.noreply.github.com>
2025-07-26 22:21:35 +0530

    Add files via upload
    Write a program to print area of circle

---
+++ Program11.java
@@ -0,0 +1,12 @@
+public class Program11{
+public static void main(String args[]){
+    int radius=5;
+    float area_of_circle= 3.14f*(radius*radius);
+    System.out.println(area_of_circle);
+   
+
+
+}
+
+
+}
