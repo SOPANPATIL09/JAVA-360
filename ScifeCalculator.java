fa056fb9f6f2767eb7eeb86ca3449e26ad3cfff3
sopan pramod patil <143089038+SOPANPATIL09@users.noreply.github.com>
2025-12-11 17:40:50 +0000

    inheritance example

---
+++ ScifeCalculator.java
@@ -0,0 +1,16 @@
+ class ScifiCalculator extends Calculator{
+         public double square(double a){
+            return a*a;
+        }
+        public double cube(double a){
+            return a*a*a;
+
+        }
+        public double squareroot(double a){
+            return Math.sqrt(a);
+        }
+        public double cuberoot(double a){
+            return Math.cbrt(a);
+        }
+
+    }
